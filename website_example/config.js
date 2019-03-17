d59640e13fea32d9d9bdb72eeb5ea508db2c5fd6
ryooriginal <48225684+ryooriginal@users.noreply.github.com>
2019-03-17 04:19:58 +0200

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -6,8 +6,8 @@ var discord = "https://discordapp.com/invite/YourPool";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var blockchainExplorer = "http://explorer.ryo-original.org/{symbol}/block/{id}";
+var transactionExplorer = "http://explorer.ryo-original.org/{symbol}/transaction/{id}";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
